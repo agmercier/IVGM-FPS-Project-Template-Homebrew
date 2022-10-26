d5cbec67c230f10b912d622b1d78f2342d784fc2
kahming0 <105551377+kahming0@users.noreply.github.com>
2022-10-26 12:14:23 +0200

    Changed DeathScript to respawn the player to initial starting point; instead of outright destroying the player
    title

--- Assets/02_Student Folders/KahmingWong/Scripts/DeathScript.cs
+++ Assets/02_Student Folders/KahmingWong/Scripts/DeathScript.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeathScript : MonoBehaviour {
 	public GameObject Player;
 
 	private void OnTriggerEnter(Collider other) {
 		if (other.gameObject == Player) {
-			Destroy(Player);
+			Scene currentScene = SceneManager.GetActiveScene();
+			SceneManager.LoadScene(currentScene.name);
 		}
 	}
 }
